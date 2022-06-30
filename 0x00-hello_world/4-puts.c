3ef48067a722d82ee36851e2c12a284f9d649870
Giddyspurz <iamroot2005@protonmail.com>
2022-06-30 04:13:08 -0700

    Programming is like building a multilingual puzzle

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *main - function puts that prints the specific phrase
+ *
+ *Return: Always 0 (End)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
