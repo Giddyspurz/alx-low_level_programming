caa0cfadc8abcd538314cae19e0d9ab772530b99
Giddyspurz <iamroot2005@protonmail.com>
2022-07-05 02:27:09 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,21 @@
-#include"main.h"
+#include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * main - Putchar
+ * _putchar print the characters given
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	int index;
-	char putchar_arr[] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
-
-	for (index = 0; index <= 7; index++)
-	{
-	_putchar(putchar_arr[index]);
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 	return (0);
 }
