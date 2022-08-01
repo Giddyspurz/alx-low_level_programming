6a2dc52e99c945bfae1cba30531a271bdb3f0f68
Giddyspurz <iamroot2005@protonmail.com>
2022-08-01 22:27:28 +0300

    0x0D-preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#ifndef MAIN_H
+#define MAIN_H
+
+#define NAME __FILE__
+
+#endif
+/**
+ * main - entry point
+ *
+ * Return: success(0)
+ */
+int main(void)
+{
+	printf("%s\n", NAME);
+
+	return (0);
+}
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,6 @@
+#ifndef SUM_H
+#define SUM_H
+
+#define SUM(A, B) (A + B)
+
+#endif
